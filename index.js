599a3ad4627e7bea89d92e43ee2650f11bc23ded
Lasse Haverinen <lasse.haverinen@oamk.fi>
2020-02-03 15:15:37 +0200

    Added api key and http basic demos

--- index.js
+++ index.js
@@ -2,68 +2,103 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const todos = require('./services/todos');
+const users = require('./services/users');
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
-
-/*
-console.log(todos.getAllTodos());
-todos.insertTodo("Test", (new Date()).toISOString(), 1);
-console.log(todos.getAllTodos());
-todos.insertTodo("Test 2", (new Date()).toISOString(), 2);
-console.log(todos.getAllTodos());
-console.log(todos.getAllUserTodos(2));
-*/
-
-
-
 app.use(express.json());
-
 
 app.get('/', (req, res) => res.send('This route is not protected'));
 
-app.post('/apiKeyLogin', (req, res) => {
-  if(req.body.username === undefined || req.body.password === undefined)
-  {
-    res.sendStatus(400);
-    return;
-  }
-  res.json(users);
-})
 
-
-
-app.post('/users', (req, res) => {
-  console.log('Hello /users');
-  
-  if(req.body.hasOwnProperty('email'))
-  {
-    // I accept or validate more
-    const result = {
-      id: 234622
-    };
-
-    users.push({
-      id: users.length + 1,
-      username: req.body.username,
-      name: req.body.name,
-      dateOfBirth: req.body.dateOfBirth,
-      address: req.body.address,
-      city: req.body.city,
-      country: req.body.country,
-      email: req.body.email
-    })
-  
-    res.status(201).json(result);
-  }
-  else
+/*********************************************
+ * API KEY DEMO
+ ********************************************/
+app.get('/apiKeyGetKey/:userId', (req, res) => {
+  const userId = req.params.userId;
+  let apiKey = users.getApiKey(userId);
+  if(apiKey === false) // user not found
   {
     res.sendStatus(400);
   }
-
-
-  
+  if(apiKey === null)
+  {
+    apiKey = users.resetApiKey(userId)
+  }
+  res.json({
+    apiKey
+  })
 });
+
+function checkForApiKey(req, res, next)
+{  
+  const receivedKey = req.get('X-Api-Key');
+  if(receivedKey === undefined)
+  {
+    return res.status(400).json({ reason: "X-Api-Key header missing"});
+  }
+
+  const user = users.getUserWithApiKey(receivedKey);
+  if(user === undefined)
+  {
+    return res.status(400).json({ reason: "Incorrect api key"});
+  }
+
+  req.user = user;
+
+  // pass the control to the next handler in line
+  next();  
+}
+
+app.get('/apiKeyProtectedResource', checkForApiKey, (req, res) => {
+  res.json({
+    yourResource: "foo"
+  })
+});
+
+/*********************************************
+ * HTTP Basic Authentication
+ ********************************************/
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+
+    const user = users.getUserByName(username);
+    if(user == undefined)
+    {
+      // Username not found
+      console.log("HTTP Basic username not found");
+      return done(null, false, { message: "HTTP Basic username not found" });
+    }
+
+    /* Verify password match, note that the password here is in plaintext.
+       DO NOT EVER STORE PASSWORDS in plaintexts */
+    if(user.password !== password) 
+    {
+      // Password does not match
+      console.log("HTTP Basic password not matching username");
+      return done(null, false, { message: "HTTP Basic password not found" });
+    }
+    return done(null, user);
+  }
+));
+
+app.get('/httpBasicProtectedResource',
+        passport.authenticate('basic', { session: false }),
+        (req, res) => {
+  
+  res.json({
+    yourProtectedResource: "profit"
+  });
+});
+
+
+/*********************************************
+ * JWT authentication
+ ********************************************/
+let JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- services/users.js
+++ services/users.js
@@ -1,17 +1,41 @@
+const uuidv4 = require('uuid/v4');
 
 let users = [
   {
     id: 1,
     userName: 'tester',
-    password: 'testerpassword' // Example only! Do NOT EVER STORE PASSWORD ANYWHERE IN PLAINTEXT!
+    password: 'testerpassword', // Example only! Do NOT EVER STORE PASSWORD ANYWHERE IN PLAINTEXT!
+    validApiKey: null
   },
   {
     id: 2,
     userName: "johndoe",
-    password: 'johndoepassword' // Example only! Do NOT EVER STORE PASSWORD ANYWHERE IN PLAINTEXT!
+    password: 'johndoepassword', // Example only! Do NOT EVER STORE PASSWORD ANYWHERE IN PLAINTEXT!
+    validApiKey: null
   },
 ];
 
 module.exports = {
   getUserByName: (username) => users.find(u => u.userName == username),
+  resetApiKey: (userId) => {
+    const user = users.find(u => u.id == userId);
+    if(user === undefined)
+    {
+      return false
+    }
+
+    user.validApiKey = uuidv4();
+    return user.validApiKey;
+  },
+  getApiKey: (userId) => {
+    const user = users.find(u => u.id == userId);
+    if(user === undefined)
+    {
+      return false
+    }
+    
+    return user.validApiKey;
+  },
+  getUserWithApiKey: (apiKey) => users.find(u => u.validApiKey == apiKey)
+
 }
